d75de150f07953b69c61303f75e7a175322a8e43
nagendradhanu08 <nagendrad502@gmail.com>
2018-04-12 16:39:53 +0530

    httpclient,odule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs/Rx';
+import { error } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  baseUrl: string ="https://api.github.com/";
+  userName: string ="tektutorialsHub";
+  loading: boolean=false;
+  repos: any[];
+  constructor(public http: HttpClient)
+   {
+    
+  }
+  public getRepos()
+   {
+
+    this.loading = true;
+    this.http.get<any[]>(this.baseUrl+'users/'+this.userName+'/repos')
+             .subscribe(data => {
+                 this.repos = data;
+                 this.loading = false;
+            },
+            error => {
+                this.repos = error;
+                this.loading = false;
+            }
+        );
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
